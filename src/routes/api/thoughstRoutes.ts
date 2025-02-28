1db72aeb715bf076bc9f2bba9aacfa5f3587fe5a
Elhadj Bah <[elhadjamadoubah@gmail.com]>
2025-02-27 18:54:24 -0700

    add thoughtsRoutes

---
+++ src/routes/api/thoughstRoutes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+const router = Router();
+
+import {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} from "../../controllers/thoughtsControllers.js";
+
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id
+
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtID/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtID/reactions/:reactionID
+
+router.route("/:thoughtID/reactions/:reactionID").delete(deleteReaction);
+
+export default router;
